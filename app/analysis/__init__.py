68a326c3c79b5b12924cc61b1a034875376d377c
Jesús Ramos Membrive <ramos.membrive@gmail.com>
2025-08-24 23:25:28 +0200

    feat: add SPC charts for historical analysis

--- app/analysis/__init__.py
+++ app/analysis/__init__.py
@@ -8,12 +8,14 @@ from . import timing
 from . import openings
 from . import endgame
 from . import longitudinal
+from . import spc
 from .engine import ChessAnalysisEngine, prepare_moves_dataframe
 
 from .quality   import aggregate_quality_features
 from .timing    import aggregate_time_features
 from .openings  import aggregate_opening_features
 from .endgame   import aggregate_endgame_features
+from .spc       import compute_spc
 
 __all__ = [
     'quality',
@@ -21,10 +23,12 @@ __all__ = [
     'openings',
     'endgame',
     'longitudinal',
+    'spc',
     'prepare_moves_dataframe',
     'ChessAnalysisEngine',
     'aggregate_quality_features',
     'aggregate_time_features',
     'aggregate_opening_features',
     'aggregate_endgame_features',
+    'compute_spc',
 ]
